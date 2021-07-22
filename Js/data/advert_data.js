4212629a277a9b876a19264be1f26a8f6b951da3
Hongleang Lim <lim.hongleang@gmail.com>
2021-07-22 22:41:35 +1000

    Structuring html and setting scss variables

---
+++ Js/data/advert_data.js
@@ -0,0 +1,24 @@
+const ADVERT_DATA = [
+  {
+    "title": "Transform your brand",
+    "description": "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
+    "link": "Learn more"
+  },
+  {
+    "title": "Stand out to the right audience",
+    "description": "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.",
+    "link": "Learn more"
+  },
+  {
+    "title": "Graphic design",
+    "description": "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.",
+    "link": "Learn more"
+  },
+  {  
+    "title": "Photography",
+    "description": "Increase your credibility by getting the most stunning, high-quality photos that improve your business image.",
+    "link": "Learn more"
+  }  
+];
+
+  
