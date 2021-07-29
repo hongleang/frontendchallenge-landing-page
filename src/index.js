621322140a7ab9f3b4183d72ffde0121f053f135
hongleang <lim.hongleang@gmail.com>
2021-07-29 21:33:21 +1000

    completed the challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import  { ConstructingAdvertItem, ConstructingServices, ConstructingTestimonials } from './scripts/ContructingDom';
+import Modal from './scripts/Modal';
 
+Modal();
 ConstructingAdvertItem();
 ConstructingServices();
 ConstructingTestimonials();
