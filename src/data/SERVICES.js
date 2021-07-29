b545fc74583cb45aad8510456f839b4c540acd98
hongleang <lim.hongleang@gmail.com>
2021-07-29 22:53:57 +1000

    Fix img file path

--- src/data/SERVICES.js
+++ src/data/SERVICES.js
@@ -1,15 +1,15 @@
 const SERVICES = [{
   "title": "Graphic design",
   "description": "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.",
-  "desktopImg": "../../images/desktop/image-graphic-design.jpg",
-  "mobileImg": "../../images/mobile/image-graphic-design.jpg",
+  "desktopImg": "images/desktop/image-graphic-design.jpg",
+  "mobileImg": "images/mobile/image-graphic-design.jpg",
   "link": "Learn more"
 },
 {
   "title": "Photography",
   "description": "Increase your credibility by getting the most stunning, high-quality photos that improve your business image.",
-  "desktopImg": "../../images/desktop/image-photography.jpg",
-  "mobileImg": "../../images/mobile/image-photography.jpg",
+  "desktopImg": "images/desktop/image-photography.jpg",
+  "mobileImg": "images/mobile/image-photography.jpg",
   "link": "Learn more"
 }
 ];
