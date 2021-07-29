0c2fe8319be8cf4ce7947421fcca1782bd7e2507
hongleang <lim.hongleang@gmail.com>
2021-07-29 21:51:55 +1000

    fix image path

--- src/data/ADVERTS.js
+++ src/data/ADVERTS.js
@@ -2,15 +2,15 @@ const ADVERTS_DATA = [
     {
       "title": "Transform your brand",
       "description": "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
-      "desktopImg": "/images/desktop/image-transform.jpg",
-      "mobileImg": "/images/mobile/image-transform.jpg",
+      "desktopImg": "../../images/desktop/image-transform.jpg",
+      "mobileImg": "../../images/mobile/image-transform.jpg",
       "link": "Learn more"
     },
     {
       "title": "Stand out to the right audience",
       "description": "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places.",
-      "desktopImg": "/images/desktop/image-stand-out.jpg",
-      "mobileImg": "/images/mobile/image-stand-out.jpg",
+      "desktopImg": "../../images/desktop/image-stand-out.jpg",
+      "mobileImg": "../../images/mobile/image-stand-out.jpg",
       "link": "Learn more"
     },
     
